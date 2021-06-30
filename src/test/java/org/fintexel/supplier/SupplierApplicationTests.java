3f0ba58a95bafc354c96a7ca583c6301b8371db3
sunchulapraveen <sunchulapraveen1@gmail.com>
2021-06-30 12:47:48 +0530

    project setup

---
+++ src/test/java/org/fintexel/supplier/SupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fintexel.supplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
