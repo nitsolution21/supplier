220098dd674c1ae6b94c4bcd2a2c8492477f0e61
nitvenkatesh <c.venkatesh@nitsolution.in>
2021-07-03 00:36:58 +0530

    Added YML Configurations to application

--- src/main/java/org/fintexel/supplier/SupplierApplication.java
+++ src/main/java/org/fintexel/supplier/SupplierApplication.java
@@ -1,14 +1,31 @@
 package org.fintexel.supplier;
 
+import org.fintexel.supplier.config.YMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SupplierApplication {
+public class SupplierApplication implements CommandLineRunner {
+	
+	@Autowired
+	private YMLConfig myConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupplierApplication.class, args);
-		System.out.println("test");
+		System.out.println("In Main Method");
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("-------------------------------------");
+		System.out.println("name : "+myConfig.getName());
+		System.out.println("environment : "+myConfig.getEnvironment());
+		System.out.println("contextpath : "+myConfig.getContextpath());
+		System.out.println("servers: "+myConfig.getServers());
+		System.out.println("-------------------------------------");
+	}
+	
 
 }
--- src/main/java/org/fintexel/supplier/controller/VendorController.java
+++ src/main/java/org/fintexel/supplier/controller/VendorController.java
@@ -1,15 +1,21 @@
 package org.fintexel.supplier.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/vendor")
 public class VendorController {
 
 	@PostMapping("/registerVendor")
 	public String registerVendor() {
 		return "Hello";
 	}
+	
+	@GetMapping("/vendor")
+	public String getVendor() {
+		return "Hello";
+	}
+	
 }
