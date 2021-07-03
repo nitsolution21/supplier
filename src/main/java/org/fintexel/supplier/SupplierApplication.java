1f4b75a45f15a2da7e737ae2d0bb2ced23cfdef3
nitsolution2021 <praveen@nitsolution.in>
2021-07-03 23:12:50 +0530

    added Logging

--- src/main/java/org/fintexel/supplier/SupplierApplication.java
+++ src/main/java/org/fintexel/supplier/SupplierApplication.java
@@ -1,6 +1,8 @@
 package org.fintexel.supplier;
 
 import org.fintexel.supplier.config.YMLConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,12 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SupplierApplication implements CommandLineRunner {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SupplierApplication.class);
+	
 	@Autowired
 	private YMLConfig myConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupplierApplication.class, args);
 		System.out.println("In Main Method");
+		
+	
 	}
 
 	@Override
@@ -25,6 +31,13 @@ public class SupplierApplication implements CommandLineRunner {
 		System.out.println("contextpath : "+myConfig.getContextpath());
 		System.out.println("servers: "+myConfig.getServers());
 		System.out.println("-------------------------------------");
+		
+		LOGGER.info("-------------------------------------");
+		LOGGER.info("name : "+myConfig.getName());
+		LOGGER.info("environment : "+myConfig.getEnvironment());
+		LOGGER.info("contextpath : "+myConfig.getContextpath());
+		LOGGER.info("servers: "+myConfig.getServers());
+		LOGGER.info("-------------------------------------");
 	}
 	
 
--- src/main/java/org/fintexel/supplier/controller/VendorController.java
+++ src/main/java/org/fintexel/supplier/controller/VendorController.java
@@ -1,5 +1,8 @@
 package org.fintexel.supplier.controller;
 
+import org.fintexel.supplier.SupplierApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VendorController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(VendorController.class);
 
 	@PostMapping("/registerVendor")
 	public String registerVendor() {
+		LOGGER.info("Inside - VendorController.registerVendor()");
 		return "Hello";
 	}
 	
 	@GetMapping("/vendor")
 	public String getVendor() {
+		LOGGER.info("Inside - VendorController.getVendor()");
 		return "Hello";
 	}
 	
