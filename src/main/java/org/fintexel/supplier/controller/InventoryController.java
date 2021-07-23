97d6a0676a07672efea7b29f6958d700b8b50890
soumendolui <soumendolui077@gmail.com>
2021-07-23 17:50:16 +0530

    put api for category done from soumen at 23-07-21_05-50

--- src/main/java/org/fintexel/supplier/controller/InventoryController.java
+++ src/main/java/org/fintexel/supplier/controller/InventoryController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -93,6 +94,46 @@ public class InventoryController {
 		}
 
 	}
+	
+	
+	
+	@PutMapping("/vendor/category/{id}")
+	public ItemCategory updateCategory(@PathVariable() Long id, @RequestBody ItemCategory itemCategory, @RequestHeader(name = "Authorization") String token) {
+		LOGGER.info("Inside - InventoryController.updateCategory()");
+		try {
+			boolean flag =true;
+			String loginSupplierCode = loginUserDetails.getLoginSupplierCode(token);
+			if ((fieldValidation.isEmpty(itemCategory.getCategoryName()))) {
+				if (!loginSupplierCode.equals(null)) {
+					ItemCategory category = new ItemCategory();
+					Optional<ItemCategory> findById = itemCategoryRepo.findById(id);
+					if (findById.isPresent()) {
+						if(findById.get().getSupplierCode().equals(loginSupplierCode)) {
+							category.setCategoryName(itemCategory.getCategoryName());
+							category.setSupplierCode(loginSupplierCode);
+							return itemCategoryRepo.save(category);
+						}else {
+							throw new VendorNotFoundException("You don't have permission to update this category");
+						}
+					}else {
+						throw new VendorNotFoundException("Category Name Not Exist");
+
+						
+					}
+				} else {
+					throw new VendorNotFoundException("Token not valid");
+				}
+			} else {
+				throw new VendorNotFoundException("Validation Error");
+			}
+
+		} catch (Exception e) {
+			throw new VendorNotFoundException(e.getMessage());
+		}
+
+	}
+
+
 
 	@PostMapping("/vendor/subCategory")
 	public ItemSubCategory addSubCategory(@RequestBody ItemSubCategory itemSubCategory, @RequestHeader(name = "Authorization") String token) {
@@ -120,6 +161,41 @@ public class InventoryController {
 						category.setSubCategoryName(itemSubCategory.getSubCategoryName());
 						category.setSupplierCode(loginSupplierCode);
 						return itemSubCategoryRepo.save(category);
+					}
+				} else {
+					throw new VendorNotFoundException("Token not valid");
+				}
+			} else {
+				throw new VendorNotFoundException("Validation Error");
+			}
+		} catch (Exception e) {
+			throw new VendorNotFoundException(e.getMessage());
+		}
+	}
+	
+	
+	
+	@PutMapping("/vendor/subCategory/{id}")
+	public ItemSubCategory updateSubCategory(@PathVariable() Long id,@RequestBody ItemSubCategory itemSubCategory, @RequestHeader(name = "Authorization") String token) {
+		LOGGER.info("Inside - updateSubCategory " + itemSubCategory);
+		try {
+			String loginSupplierCode = loginUserDetails.getLoginSupplierCode(token);
+			if ((fieldValidation.isEmpty(itemSubCategory.getSubCategoryName())) && fieldValidation.isEmpty(itemSubCategory.getCategoryId())) {
+				if (!loginSupplierCode.equals(null)) {
+					Optional<ItemSubCategory> findById = itemSubCategoryRepo.findById(id);
+					ItemSubCategory category = new ItemSubCategory();
+					if (findById.isPresent()) {
+						if(findById.get().getSupplierCode().equals(loginSupplierCode)) {
+							category.setCategoryId(itemSubCategory.getCategoryId());
+							category.setSubCategoryName(itemSubCategory.getSubCategoryName());
+							category.setSupplierCode(loginSupplierCode);
+							return itemSubCategoryRepo.save(category);
+						}else {
+							throw new VendorNotFoundException("You don't have permission to update this category");
+						}
+						
+					} else {
+						throw new VendorNotFoundException("Sub Category Name Not Exist");
 					}
 				} else {
 					throw new VendorNotFoundException("Token not valid");
