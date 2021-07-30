b825dd0c62e06c676ec22916362d610133f27af2
shantanu1998a <shantanurong44@gmail.com>
2021-07-30 18:32:08 +0530

    add customer roll master and customer register customer user repo and entity class

---
+++ src/main/java/org/fintexel/supplier/customerrepository/RolesMasterRepo.java
@@ -0,0 +1,8 @@
+package org.fintexel.supplier.customerrepository;
+
+import org.fintexel.supplier.customerentity.RolesMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesMasterRepo extends JpaRepository<RolesMaster, Long> {
+
+}
