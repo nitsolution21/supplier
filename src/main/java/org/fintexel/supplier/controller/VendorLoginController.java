ffe76f1992330ea9552d3f6f1d30beedb3eeb1a1
shantanu1998a <shantanurong44@gmail.com>
2021-08-03 18:18:35 +0530

    fix auto complete for forgot password

--- src/main/java/org/fintexel/supplier/controller/VendorLoginController.java
+++ src/main/java/org/fintexel/supplier/controller/VendorLoginController.java
@@ -66,7 +66,7 @@ import org.springframework.web.client.RestTemplate;
 public class VendorLoginController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(VendorLoginController.class);
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
@@ -92,20 +92,20 @@ public class VendorLoginController {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	FlowableRegistrationRepo flowableRegistrationRepo;
-	
+
 	@Autowired
-	ForgotPasswordRepo forgotPasswordRepo; 
-	
+	ForgotPasswordRepo forgotPasswordRepo;
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	private String taskId;
-	
+
 	@Autowired
-	private FlowableFormRepo flowableFormRepo; 
+	private FlowableFormRepo flowableFormRepo;
 
 	@PostMapping("/login")
 	public ResponseEntity<?> venderLogin(@RequestBody VendorLogin vendorLogin) {
@@ -123,7 +123,8 @@ public class VendorLoginController {
 
 		Optional<VendorRegister> findByUsername = vendorRegisterRepo.findByUsername(vendor.getUsername());
 
-		return ResponseEntity.ok(new LoginResponce(vendor.getUsername(), token, findByUsername.get().getRegisterId(), findByUsername.get().getSupplierCompName()));
+		return ResponseEntity.ok(new LoginResponce(vendor.getUsername(), token, findByUsername.get().getRegisterId(),
+				findByUsername.get().getSupplierCompName()));
 	}
 
 	@GetMapping("/vendorLogin")
@@ -237,26 +238,28 @@ public class VendorLoginController {
 		}
 
 	}
-	
+
 	@PostMapping("/forgotPassword")
 	public Object forgotPassword(@RequestBody ForgotPasswordRequestEntity forgotPasswordRequestEntity) {
 		try {
-			if (fieldValidation.isEmpty(forgotPasswordRequestEntity.getEmail()) && fieldValidation.isEmpty(forgotPasswordRequestEntity.getUrl())) {
+			if (fieldValidation.isEmpty(forgotPasswordRequestEntity.getEmail())
+					&& fieldValidation.isEmpty(forgotPasswordRequestEntity.getUrl())) {
 				if (fieldValidation.isEmail(forgotPasswordRequestEntity.getEmail())) {
-					Optional<VendorRegister> findByEmail = registerRepo.findByEmail(forgotPasswordRequestEntity.getEmail());
+					Optional<VendorRegister> findByEmail = registerRepo
+							.findByEmail(forgotPasswordRequestEntity.getEmail());
 					if (!findByEmail.isPresent()) {
 						throw new VendorNotFoundException("We could't foumd your email id");
-					} 
+					}
 					String token = java.util.UUID.randomUUID().toString();
 //					DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 //					LocalDateTime now = LocalDateTime.now();
 //					
 //					SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");  
-					 Date date = new Date();
+					Date date = new Date();
 					try {
 						Optional<FlowableRegistration> findByAuthorAndTitle = flowableRegistrationRepo
 								.findByAuthorAndTitle("forgot_supplier_pwd");
-						
+
 						RestTemplate restTemplate = new RestTemplate();
 						HttpHeaders headers = new HttpHeaders();
 						headers.setContentType(MediaType.APPLICATION_JSON);
@@ -271,73 +274,72 @@ public class VendorLoginController {
 						JSONObject jsonObject = new JSONObject(response.getBody());
 						Map<String, Object> mapp = new HashMap<>();
 						map.put("processInstanceId", (String) jsonObject.get("id"));
-						
-						//LOGGER.info("Task ID for forgot password - "+(String) jsonObject.get("id"));
-						
+
+						// LOGGER.info("Task ID for forgot password - "+(String) jsonObject.get("id"));
+
 						HttpEntity<Map<String, Object>> request = new HttpEntity<>(map, headers);
 						ResponseEntity<String> exchange = restTemplate.exchange(
 								"http://65.2.162.230:8080/flowable-rest/service/query/tasks", HttpMethod.POST, request,
 								String.class, 1);
 						JSONObject jsonObject1 = new JSONObject(exchange.getBody());
-						
+
 						JSONArray array = new JSONArray(jsonObject1.get("data").toString());
 						JSONArray arrayy = new JSONArray();
-						
-						
+
 						JSONObject forgotPwdEmail = new JSONObject();
 						forgotPwdEmail.put("name", "supplierforgotemailid");
 						forgotPwdEmail.put("scope", "local");
 						forgotPwdEmail.put("type", "string");
 						forgotPwdEmail.put("value", forgotPasswordRequestEntity.getEmail());
 						arrayy.put(forgotPwdEmail);
-						
+
 						JSONObject forgotPwdLink = new JSONObject();
-						forgotPwdLink.put("name", "forgotpwdlink"); 
+						forgotPwdLink.put("name", "forgotpwdlink");
 						forgotPwdLink.put("scope", "local");
 						forgotPwdLink.put("type", "string");
 						forgotPwdLink.put("value", forgotPasswordRequestEntity.getUrl() + "/" + token);
 						arrayy.put(forgotPwdLink);
-						
+
 						HttpEntity<String> entityy = new HttpEntity<String>(arrayy.toString(), headers);
 						ResponseEntity<String> response2 = restTemplate.exchange(
 								"http://65.2.162.230:8080/flowable-rest/service/runtime/tasks/"
 										+ array.getJSONObject(0).get("id") + "/variables",
 								HttpMethod.POST, entityy, String.class, 1);
-						
-						taskId = (String)array.getJSONObject(0).get("id");
-						
-						LOGGER.info("Taks ID for forgot password " +array.getJSONObject(0).get("id"));
-						
+
+						taskId = (String) array.getJSONObject(0).get("id");
+
+						LOGGER.info("Taks ID for forgot password " + array.getJSONObject(0).get("id"));
+
 					} catch (Exception e) {
 					}
-					
+
 //					AUTO CLAIM  START
-					
+
 					HttpHeaders headers = new HttpHeaders();
 					headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-					MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+					MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
 					map.add("j_username", "indexer");
 					map.add("j_password", "123");
 					map.add("submit", "Login");
 					map.add("_spring_security_remember_me", "true");
-					HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
-					ResponseEntity<String> response = restTemplate.postForEntity( "http://65.2.162.230:8080/DB-idm/app/authentication", request , String.class );
+					HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(
+							map, headers);
+					ResponseEntity<String> response = restTemplate
+							.postForEntity("http://65.2.162.230:8080/DB-idm/app/authentication", request, String.class);
 					JSONObject claimcookie = new JSONObject(response.getHeaders());
-					String replace = claimcookie.get("Set-Cookie").toString().replace("[", "").replace("]", "").replace("\"", "");						
+					String replace = claimcookie.get("Set-Cookie").toString().replace("[", "").replace("]", "")
+							.replace("\"", "");
 					HttpHeaders requestHeaders = new HttpHeaders();
 					requestHeaders.add("Cookie", replace);
 					HttpEntity requestEntity = new HttpEntity(null, requestHeaders);
 					ResponseEntity rssResponse = restTemplate.exchange(
-					    "http://65.2.162.230:8080/DB-task/app/rest/tasks/"+taskId + "/action/claim",
-					    HttpMethod.PUT,
-					    requestEntity,
-					    String.class);
-					
+							"http://65.2.162.230:8080/DB-task/app/rest/tasks/" + taskId + "/action/claim",
+							HttpMethod.PUT, requestEntity, String.class);
+
 //					AUTO CLAIM  END
-					
+
 //					AUTO COMPLEATE START
 
-						
 					HttpHeaders header = new HttpHeaders();
 					header.add("Cookie", replace);
 					header.setContentType(MediaType.APPLICATION_JSON);
@@ -345,27 +347,22 @@ public class VendorLoginController {
 					JSONObject autoCompleate = new JSONObject();
 					autoCompleate.put("taskIdActual", taskId);
 					autoCompleate.put("supplierforgotemailid", forgotPasswordRequestEntity.getEmail());
-					autoCompleate.put("forgotpwdlink", forgotPasswordRequestEntity.getUrl()+taskId);
-					JSONObject mapp = new JSONObject(); 
+					autoCompleate.put("forgotpwdlink", forgotPasswordRequestEntity.getUrl() + "/" + token + taskId);
+					JSONObject mapp = new JSONObject();
 					Optional<FlowableForm> findByFromId = flowableFormRepo.findByFromId("frmforgotpwdsupplier");
 					mapp.put("formId", findByFromId.get().getId());
-					mapp.put("values", autoCompleate); 
-					System.out.println("Body  "+mapp);
-					System.out.println("headers  "+header);
-					HttpEntity<Map<String, Object>> entity = new HttpEntity(mapp.toString(),header);			
+					mapp.put("values", autoCompleate);
+					System.out.println("Body  " + mapp);
+					System.out.println("headers  " + header);
+					HttpEntity<Map<String, Object>> entity = new HttpEntity(mapp.toString(), header);
 					ResponseEntity rssResponsee = restTemplate.exchange(
-							"http://65.2.162.230:8080/DB-task/app/rest/task-forms/"+taskId,
-						    HttpMethod.POST	,
-						    entity,
-						    String.class);
+							"http://65.2.162.230:8080/DB-task/app/rest/task-forms/" + taskId, HttpMethod.POST, entity,
+							String.class);
 //					System.out.print("Result  "+rssResponsee.getHeaders());
-					System.out.print("Result  "+rssResponsee.getHeaders());
-					
-					
-					
-					
+					System.out.print("Result  " + rssResponsee.getHeaders());
+
 //					AUTO COMPLEATE END
-					
+
 					ForgotPassword forgotPassword = new ForgotPassword();
 					forgotPassword.setEmail(forgotPasswordRequestEntity.getEmail());
 					forgotPassword.setToken(token);
@@ -373,8 +370,7 @@ public class VendorLoginController {
 					forgotPassword.setStatus("MAILSEND");
 					forgotPasswordRepo.save(forgotPassword);
 					return "Please check your mail";
-					
-					
+
 				} else {
 					throw new VendorNotFoundException("Provide correct email id");
 				}
@@ -385,20 +381,25 @@ public class VendorLoginController {
 			throw new VendorNotFoundException(e.getMessage());
 		}
 	}
-	
+
 	@PostMapping("/recoverPassword")
 	public Object recoverPassword(@RequestBody RecoverPassword recoverPassword) {
 		try {
-			if (fieldValidation.isEmpty(recoverPassword.getNewPasswoed()) && fieldValidation.isEmpty(recoverPassword.getToken())) {
+			if (fieldValidation.isEmpty(recoverPassword.getNewPasswoed())
+					&& fieldValidation.isEmpty(recoverPassword.getToken())) {
 				Optional<ForgotPassword> findByToken = forgotPasswordRepo.findByToken(recoverPassword.getToken());
 				if (findByToken.isPresent()) {
 					if (!findByToken.get().getStatus().equals("EXPIRE")) {
 						Date date = new Date();
-						if (date.getYear() == findByToken.get().getCreatedOn().getYear() && date.getMonth() == findByToken.get().getCreatedOn().getMonth() && date.getDate() == findByToken.get().getCreatedOn().getDate() && date.getHours() == findByToken.get().getCreatedOn().getHours()) {
+						if (date.getYear() == findByToken.get().getCreatedOn().getYear()
+								&& date.getMonth() == findByToken.get().getCreatedOn().getMonth()
+								&& date.getDate() == findByToken.get().getCreatedOn().getDate()
+								&& date.getHours() == findByToken.get().getCreatedOn().getHours()) {
 							int expireDate = date.getMinutes() - findByToken.get().getCreatedOn().getMinutes();
-							
+
 							if (expireDate <= 5) {
-								Optional<VendorRegister> findByEmail = registerRepo.findByEmail(findByToken.get().getEmail());
+								Optional<VendorRegister> findByEmail = registerRepo
+										.findByEmail(findByToken.get().getEmail());
 								if (findByEmail.isPresent()) {
 									VendorRegister register = new VendorRegister();
 									register.setPassword(passwordEncoder.encode(recoverPassword.getNewPasswoed()));
@@ -410,7 +411,7 @@ public class VendorLoginController {
 									register.setProcessId(findByEmail.get().getProcessId());
 									register.setTaskId(findByEmail.get().getTaskId());
 									VendorRegister save = registerRepo.save(register);
-									//save.setPassword(recoverPassword.getNewPasswoed());
+									// save.setPassword(recoverPassword.getNewPasswoed());
 									ForgotPassword forgotPassword = new ForgotPassword();
 									forgotPassword.setCreatedOn(findByToken.get().getCreatedOn());
 									forgotPassword.setEmail(findByToken.get().getEmail());
@@ -419,7 +420,7 @@ public class VendorLoginController {
 									forgotPassword.setStatus("EXPIRE");
 									forgotPasswordRepo.save(forgotPassword);
 									return "password has been changed!! please remember your password";
-									
+
 								} else {
 									throw new VendorNotFoundException("Can't Find your emai id");
 								}
@@ -443,5 +444,4 @@ public class VendorLoginController {
 		}
 	}
 
-	
 }
