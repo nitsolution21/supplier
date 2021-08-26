df27835c3e3a93d3015d9d5b698475f3a14525c1
nitsolution2021 <praveen@nitsolution.in>
2021-08-26 16:59:21 +0530

    create all repo call for po

---
+++ src/main/java/org/fintexel/supplier/customerrepository/PurchesOrderAttachmentRepo.java
@@ -0,0 +1,8 @@
+package org.fintexel.supplier.customerrepository;
+
+import org.fintexel.supplier.customerentity.PurchesOrderAttachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchesOrderAttachmentRepo extends JpaRepository<PurchesOrderAttachment, Long>{
+
+}
