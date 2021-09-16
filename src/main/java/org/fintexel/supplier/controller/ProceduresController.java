cea95236bb61994ae2fd95a3ffb59ceff24b50f7
saikatdhara346@gmail.com <saikatdhara346@gmail.com>
2021-09-16 16:48:49 +0530

    remove cross from store proc contoller

--- src/main/java/org/fintexel/supplier/controller/ProceduresController.java
+++ src/main/java/org/fintexel/supplier/controller/ProceduresController.java
@@ -14,10 +14,12 @@ import org.fintexel.supplier.procedurerepo.OverallPoAmountRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProceduresController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProceduresController.class);
