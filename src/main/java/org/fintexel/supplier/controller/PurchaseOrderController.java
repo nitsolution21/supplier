f78a547476b8f442e50fb33dcd03ec5f736807a9
soumendoluinit <soumen.dolui@nitsolution.in>
2021-10-19 12:12:37 +0530

    daily push/pull

--- src/main/java/org/fintexel/supplier/controller/PurchaseOrderController.java
+++ src/main/java/org/fintexel/supplier/controller/PurchaseOrderController.java
@@ -1122,16 +1122,16 @@ public class PurchaseOrderController {
 						autoCompleteHeader.add("Cookie", coockie_);
 						autoCompleteHeader.setContentType(MediaType.APPLICATION_JSON);
 	
-						DateTimeFormatter lastLogingFormat1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+						DateTimeFormatter lastLogingFormat1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
 						LocalDateTime lastLoginNow1 = LocalDateTime.now();
-						Date lastLogin1 = new SimpleDateFormat("yyyy-MM-dd")
+						Date lastLogin1 = new SimpleDateFormat("yyyy/MM/dd")
 								.parse(lastLoginNow.format(lastLogingFormat1));
 						
 						
 						JSONObject autoCompleate = new JSONObject();
 						autoCompleate.put("taskIdActual", taskID1_);
-						autoCompleate.put("invoicemode","Manual"+lastLogin1);
-						autoCompleate.put("workunitid", "WU-"+"");
+						autoCompleate.put("invoicemode","Manual");
+						autoCompleate.put("workunitid", "WU-"+lastLogin1);
 						autoCompleate.put("taskdate", strDate);
 						autoCompleate.put("vendorname",vendorRegister.getSupplierCompName() );
 						autoCompleate.put("vendorid", vendorRegister.getRegisterId() );
