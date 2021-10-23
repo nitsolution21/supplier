46980be0a95297b9b8706805202c247ca3e0ef46
nitsolution2021 <praveen@nitsolution.in>
2021-10-23 13:56:47 +0530

    file upload for invoice

--- src/main/java/org/fintexel/supplier/helper/FileUploadHelper.java
+++ src/main/java/org/fintexel/supplier/helper/FileUploadHelper.java
@@ -11,18 +11,21 @@ import java.time.format.DateTimeFormatter;
 
 import org.fintexel.supplier.entity.FileUploadResponse;
 import org.fintexel.supplier.exceptions.VendorNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadHelper.class);
 	@Value("${file.upload-path}")
 	private String UPLOAD_DIR;
 
 	public FileUploadResponse uploadFile(MultipartFile file) {
 		
-		System.out.println("in FileUploadHelper:  "+ UPLOAD_DIR);
+		LOGGER.info("in FileUploadHelper:  "+ UPLOAD_DIR);
 
 		try {
 			String[] parts = file.getContentType().split("/");
@@ -38,7 +41,7 @@ public class FileUploadHelper {
 					StandardCopyOption.REPLACE_EXISTING);
 			Path path = Paths.get(UPLOAD_DIR + file.getOriginalFilename().split("\\.")[0] + date[0]
 					+ date[1] + date[2] + time[0] + time[1] + time[2] + "." + extension);
-			System.out.println("path "+path);
+			LOGGER.info("path "+path);
 			FileUploadResponse fileUploadResponse = new FileUploadResponse();
 			
 //			file.transferTo(new File(UPLOAD_DIR+file.getOriginalFilename().split("\\.")[0] + date[0]
